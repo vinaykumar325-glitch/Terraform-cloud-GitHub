dc0ae7729231cc8a18b847b0884ae8356d9ba6c8
c vinay kumar <chilamalavinaykumar@gmail.com>
2024-08-08 12:43:34 +0530

    Update main.tf by vinay and added tag

--- main.tf
+++ main.tf
@@ -13,7 +13,10 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "Raminfra" {
+resource "aws_instance" "vinay-infra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
 	instance_type = "t2.nano"
+tags = {
+       Name ="terra-cloud"
+  }
 }
