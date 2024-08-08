e9e5e135ae68cc2822c65a2bb148849569bdc403
c vinay kumar <chilamalavinaykumar@gmail.com>
2024-08-08 12:48:25 +0530

    Update main.tf by instance type replaced by vinay

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_instance" "vinay-infra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.nano"
+	instance_type = "t2.micro"
 tags = {
        Name ="terra-cloud"
   }
